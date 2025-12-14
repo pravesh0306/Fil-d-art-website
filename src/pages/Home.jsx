fa12a44348f57edc3c73ccfd36e3f9182405abb2
pravesh0306 <pravesh0306@github.com>
2025-12-15 00:45:06 +0400

    Adjust hero video opacity to 30%

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -352,7 +352,7 @@ const Home = () => {
           width: 100%;
           height: 100%;
           object-fit: cover;
-          opacity: 0.55;
+          opacity: 0.3;
           transform: scale(1.3);
         }
 
